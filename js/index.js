d32ee4ae6be198dfd5096749176c87f7e89663b8
Matthias <113374253+Asbestcode@users.noreply.github.com>
2023-11-08 22:40:33 +0100

    Font weights austauschen

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 const navButtons = document.querySelectorAll(".dropdown-button");
 const navBar = document.querySelector(".navbar");
-const burgerButton = document.querySelector(".burger-button");
+const menuButton = document.querySelector(".menu-button");
+const menuPath = document.getElementById("menu-path");
 const allDropdown = document.querySelectorAll(".dropdown-container");
 let activeDropdown = null;
-
-// DROPDOWN DROPDOWN DROPDOWN //
 
 function handleDropdown() {
     for (let i = 0; i < 3; i++) {
@@ -32,11 +31,19 @@ function checkWindowSize() {
     }
 }
 
-burgerButton.addEventListener("click", () => {
-    navBar.classList.toggle("active");
-})
+menuButton.addEventListener("click", () => {
+    const closeIcon = "M17.7,16.3L13.4,12l4.3-4.3c0.4-0.4,0.4-1,0-1.4c-0.4-0.4-1-0.4-1.4,0L12,10.6L7.7,6.3c-0.4-0.4-1-0.4-1.4,0 c-0.4,0.4-0.4,1,0,1.4l4.3,4.3l-4.3,4.3c-0.4,0.4-0.4,1,0,1.4c0,0,0,0,0,0c0.4,0.4,1,0.4,1.4,0c0,0,0,0,0,0l4.3-4.3l4.3,4.3 c0.4,0.4,1,0.4,1.4,0c0,0,0,0,0,0C18.1,17.3,18.1,16.7,17.7,16.3C17.7,16.3,17.7,16.3,17.7,16.3z";
+    const burgerIcon = "M21,12L21,12c0,0.6-0.4,1-1,1H4c-0.5,0-1-0.4-1-1V12c0-0.5,0.4-1,1-1H20C20.6,11,21,11.4,21,12z M20,16H4 c-0.5,0-1,0.4-1,1V17c0,0.5,0.4,1,1,1H20c0.5,0,1-0.4,1-1V17C21,16.4,20.6,16,20,16z M20,6H4C3.4,6,3,6.4,3,7V7c0,0.5,0.4,1,1,1H20 c0.5,0,1-0.4,1-1V7C21,6.4,20.6,6,20,6z";
 
-// FORM FORM FORM FORM //
+    navBar.classList.toggle("active");
+    if(navBar.classList.contains("active")) {
+        document.body.style.overflow = "hidden";
+        menuPath.setAttribute("d", closeIcon);
+    } else {
+        menuPath.setAttribute("d", burgerIcon);
+        document.body.style.overflow = "";
+    }
+})
 
 const form = document.querySelector('[data-js="form"]');
 const dialog = document.getElementById("dialog");
@@ -50,19 +57,6 @@ form.addEventListener("submit", (event) => {
     }
     event.target.reset();
 });
-
-// function ResizePlugin(slider) {
-//     var observer = new ResizeObserver(function () {
-//       slider.update();
-//     });
-
-//     slider.on("created", () => {
-//       observer.observe(slider.container);
-//     });
-//     slider.on("destroyed", () => {
-//       observer.unobserve(slider.container);
-//     });
-// };
 
 function navigation(slider) {
     const caption = document.querySelector(".slideshow__caption");
@@ -87,15 +81,3 @@ const slider = new KeenSlider("#keen", {
 
 window.addEventListener("resize", checkWindowSize);
 checkWindowSize();
-
-
-// dialog.addEventListener("click", event => {
-//     const rect = dialog.getBoundingClientRect();
-//     if (event.clientY < rect.top || event.clientY > rect.bottom ||
-//         event.clientX < rect.left || event.clientX > rect.right) {
-//         dialog.close();
-//     }
-// });
-// closeDialogButton.addEventListener("click", function () {
-//     dialog.close();
-// });
